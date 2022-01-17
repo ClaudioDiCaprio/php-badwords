287f9032616573f2a90ee1197f6610fe553206a0
Claudio <claudiodicaprio89@gmail.com>
2022-01-17 17:24:29 +0100

    bad_words

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <?php
+    $uncensored = 'She sells sea shells by the sea shore. The shells she sells are surely seashells. So if she sells shells on the seashore, I\'m sure she sells seashore shells.';
+    $censored = str_replace($_GET['Badword'],'*****',$uncensored);
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -10,5 +12,12 @@
 </head>
 <body>
     <h1>BadWords</h1>
+
+<!-- this is the uncensored paragraph -->
+    <h2>This is the uncensored one:</h2>
+    <p><?php echo $uncensored ?></p>
+    <p>String length:<?php echo strlen($uncensored); ?></p>
+<!-- this is the uncensored paragraph's end -->
+
 </body>
 </html>
