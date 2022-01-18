81ed0d09fae02d8b6e27d08a72821c83f30ac9cd
Claudio <claudiodicaprio89@gmail.com>
2022-01-18 10:18:44 +0100

    notice_fixed_in_class

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <?php
     $uncensored = 'She sells sea shells by the sea shore. The shells she sells are surely seashells. So if she sells shells on the seashore, I\'m sure she sells seashore shells.';
+    $censored = $uncensored;
+    if( isset($_GET['Badword']) ) {
     $censored = str_replace($_GET['Badword'],'***',$uncensored);
+    }
 ?>
 <!DOCTYPE html>
 <html lang="en">
